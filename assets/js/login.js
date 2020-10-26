3c864ccecc68422635f04d09242b713485ce09bb
luoyu123321 <779231460@qq.com>
2020-10-26 18:57:57 +0800

    完成注册功能修改

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,36 +7,37 @@ $(function() {
         $('.login-box').show()
         $('.reg-box').hide()
     })
-})
 
-var form = layui.form
-form.verify({
-    pwd: [
-        /^[\S]{6,12}$/,
-        '密码必须6到12位，且不能出现空格'
-    ],
-    repwd: function(value) {
-        var pwd = $('.reg-box input[name=password]').val()
-        if (value !== pwd) {
-            return '两次密码输入不一致'
+
+    var form = layui.form
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function(value) {
+            var pwds = $('.reg-box input[name=password]').val()
+            if (value !== pwds) {
+                return '两次密码输入不一致'
+            }
         }
-    }
-})
+    })
 
-$('#form-reg').on('submit', function(e) {
-    e.preventDefault()
-    $.ajax({
-        type: 'POST',
-        url: 'http://ajax.frontend.itheima.net/api/reguser',
-        data: {
-            username: $('.reg-box input[name=username]').val(),
-            password: $('.reg-box input[name=password]').val()
-        },
-        success: function(res) {
-            if (res.status !== 0) {
+    $('#form-reg').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            data: {
+                username: $('.reg-box input[name=username]').val(),
+                password: $('.reg-box input[name=password]').val()
+            },
+            success: function(res) {
+                if (res.status !== 0) {
+                    alert(res.message)
+                }
                 alert(res.message)
             }
-            alert(res.message)
-        }
+        })
     })
 })
